b07afa65b611672844a9fa86f34ec9bd82ca5bd9
Lee <71296232+leeprky@users.noreply.github.com>
2021-08-03 14:46:21 +0100

    Update GlasscordInstallerHelper.bat

--- Glasscord/GlasscordInstallerHelper.bat
+++ Glasscord/GlasscordInstallerHelper.bat
@@ -1,8 +1,29 @@
-taskkill /IM DiscordCanary.exe /F
+mkdir %USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\Files"
+xcopy "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app" "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\Files"
+cd %USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup
+git clone https://github.com/leeprky/RestoreGit
+xcopy "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\RestoreGit\restore.bat" "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app"
+del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\RestoreGit*.*"
+for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup*.*") do rmdir /s /q "%%d"
+@Echo Off
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
+  set "DEL=%%a"
+)
+call :colorEcho a0 "BACKUP CREATED IF FILES COPIED"
+echo.
+exit
+:colorEcho
+echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1i
+timeout 3 /nobreak
 rem 
 set "FirstDir=%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\GlasscordHelper"
 set "SecondDir=%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\GlasscordHelper"
 rem 
+
 if exist "%FirstDir\" (
     rem 
     mkdir "%SecondDir%"
@@ -32,4 +53,5 @@ echo off
 findstr /v /a:%1 /R "^$" "%~2" nul
 del "%~2" > nul 2>&1i
 timeout 10 /nobreak
-close
+@Echo Off
+quit
